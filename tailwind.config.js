42aea68e5d46d050a90f6b54b6be16c9a36b68d4
Omarmoha7888 <cumartaakuur7888@gmail.com>
2026-08-15 17:01:18 +0300

    Update and rename Tailwind.config.js to tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'brand-gold': '#D4AF37', // Midabka Dahabiga ah
+        'brand-black': '#121212', // Midabka Madowga ah
+      },
+    },
+  },
+}
+
